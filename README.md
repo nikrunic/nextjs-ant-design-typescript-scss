af6f68c2bab9ca5850a45190d77296b02ac17d11
Krunal Prajapati <ghanshyamskb@gmail.com>
2021-11-10 18:42:04 +0530

    first code

---
+++ README.md
@@ -0,0 +1,26 @@
+# nextjs-ant-design-typescript-scss
+
+Next.js + Ant design + Typescript + SCSS
+
+- Including simple structure & layout
+
+## Usage
+1. Copy this repository.
+```
+git clone https://github.com/nikrunic/nextjs-ant-design-typescript-scss
+cd nextjs-ant-design-typescript-scss/
+```
+2. Install dependencies
+```
+yarn install
+```
+
+3. Run app
+``` bash
+# Dev
+yarn dev
+
+# Production
+yarn build
+yarn start
+```
---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import type { AppProps } from 'next/app';
+
+const AppLayout = dynamic(() => import('../components/Layout'), { ssr: false });
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AppLayout>
+      <Head>
+        <title>NextJs Antdesign Typescript</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
+}
